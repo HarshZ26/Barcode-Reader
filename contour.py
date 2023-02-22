5f2ebf4f0cf4ba17837dbc69131d83fec2be0ef5
Harsh Sharma <89373218+HarshZ26@users.noreply.github.com>
2023-02-22 20:03:36 +0530

    Changed directory path

--- contour.py
+++ contour.py
@@ -2,13 +2,13 @@ from init import *
 from bcode import *
 
 if __name__=="__main__":
-  for item in os.listdir(r"barcode\Input_data"):
+  for item in os.listdir(r"Input_data"):
     if item.endswith(".jpg"):
-      image = cv2.imread(r"barcode\Input_data\{}".format(item))
+      image = cv2.imread(r"Input_data\{}".format(item))
       Output,b_values = preprocess(image)
       print("The detected barcodes and their unique count.")
       pprint.pprint(b_values)
-      cv2.imwrite(os.path.join('barcode\Results','Output_'+item), Output)
+      cv2.imwrite(os.path.join('Results','Output_'+item), Output)
   cv2.imshow('Output', Output)
   cv2.waitKey(0)
   cv2.destroyAllWindows()
